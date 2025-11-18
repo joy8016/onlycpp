ed52df28be20837b44103cea3d3102b09c88edc0
joy8016 <maityjoydeb67@gmail.com>
2025-11-19 01:08:09 +0530

    cpp

---
+++ normal.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int value = 5;
+
+
+
+   value = 9;
+    cout<<value<<endl;
+
+    int a =4;
+    int b = 4;
+
+    // cout<<++a<<'\n';
+    // cout<<--b;
+//    cout << "a and equal " <<(a==b)<<endl;
+//    cout << "a and b greater than a  " <<(a<b)<<endl;
+
+//    cout<<( a!=b);
+
+cout<< "a && b is" <<(a&&b);
+
+
+//   cout<<a;
+
+    return 0;
+
+}
